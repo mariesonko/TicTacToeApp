712e947285bdf0bd0c404d63b60cc60871f54b64
mariesonko <mariesonko62@gmail.com>
2018-06-23 13:43:58 -0400

    add Game component and write the render method to render the square

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ import './index.css';
         render() {
           return (
             <button className="square">
-
+              {/*todo*/}
             </button>
           );
         }
@@ -59,3 +59,26 @@ import './index.css';
           );
         }
       }
+
+        class Game extends React.Component {
+          render() {
+            return(
+              <div className="game">
+              <div className="game-board">
+                <Board />
+              </div>
+              <div className="game-info">
+                <div>{/*status*/}</div>
+                <ol>{/* todo*/}</ol>
+                </div>
+              </div>
+            );
+          }
+        }
+
+        //==========================================
+
+          ReactDOM.render(
+            <Game />,
+            document.getElementById('root')
+          );
